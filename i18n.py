ad6ac0a84d569a3ed6f7a89dd4e717629d5e1bcf
Locke Birdsey <locke.birdsey@gmail.com>
2020-02-28 17:58:22 +0200

    Readyish to include translations and localisations for #3

--- i18n.py
+++ i18n.py
@@ -4,6 +4,5 @@ from pathlib import Path
 from core import resource_path
 
 localedir = Path.joinpath(Path(__file__).parent.absolute(), resource_path('locale'))
-print("**************************Locale directory: " + str(localedir))
 translate = gettext.translation('messages', localedir=localedir, fallback=True)
 _ = translate.gettext
